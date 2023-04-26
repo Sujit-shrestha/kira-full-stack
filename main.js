66495e0af442f8d1c574c13e061fe4da8cfa7138
sujit <sujitshrestha0004@gmail.com>
2023-04-26 18:14:41 +0545

    Added functionalities of signup , login and its validations so some sort

--- main.js
+++ main.js
@@ -1,27 +1,118 @@
-const express = require('express')
-const app = express()
-const port = 3001
 
-app.post('/signup', (req, res) => {
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+const port = 3000
+var users= [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//reads emails and password from users.json and parses it into users array
+const fs= require('fs');
+fs.readFile('users.json' , 'utf8',(err,data) => {
+if(err) {
+  console.error(err);
+  return;
+}
+ users = JSON.parse(data);
+console.log(users);
+});
+
+app.get('/signup', (req, res) => {
+  
+  res.send(`<html> 
+  <body>
+  <h1>Sign up Form</h1>
+  <form method="post" action="/signup/next">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Login</button>
+  </form>
+  </body>
+  </html>`);
   //add logic to decode body
   ///body should have email and password
-  //store email and password in the users array above only with if it doeesnt exciat
-  //return back 200 status code 
-})
+  
+  
+  
+});
 
-app.post('/login', (req, res) => {
+app.post('/signup/next' , (req , res) => {
+  const email= req.body.email;
+  const password = req.body.password;
+
+ 
+
+  //checks if user already exists IN THE ARRAY
+const existingUser =users.find(user => user.email === email);
+if(existingUser) {
+  return res.status(409).send('User already exists');
+}
+else{
+  //store email and password in the users array above only with if it doeesnt exciat
+  users.push({email , password});
+
+  //return back 200 status code 
+  res.status(200);
+  res.redirect('/login');
+}
+
+
+ 
+});
+
+app.get('/login' , (req , res) => {
+  res.send(`<html> 
+  <body>
+  <h1>lOGIN FORM</h1>
+  <form method="post" action="/login/validate">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Login</button>
+  </form>
+  </body>
+  </html>`);
+  
+});
+
+app.post('/login/validate', (req, res) => {
    //add logic to decode body
+   console.log('Validate section');
+   const email = req.body.email;
+   const password = req.body.password;
+  console.log(email);
+  console.log(password);
   ///body should have email and password
   //check if the users given email exists and check password
+  const existingUser =users.find(user=>user.email ===email);
+ 
+  
+  if(existingUser){
+    res.status(200).send('Random token:yhosaefgbSDFGD123dfsguy1w09898576gc jvmnxm,yname isndwuig.f. XAT&Io42');
+    
+  }
+  else{
+    res.status(401).send('Login failed');
+  }
   //if password is same return 200 status code
   //also send back a token(random stering)
   //IF NOT SAME return 401 status code
 
   })
 
-app.get('/quesitons' , (req,res) => {
+app.get('/questions' , (req,res) => {
   //return the user all the questions
-    res.send('Hello world!!');
+    res.send('questions');
 });
 
 app.get('/submissions', (req, res) => {
